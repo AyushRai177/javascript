1c8fd843ac94ae60bf232b51c0daa8545a8e89da
AyushRai177 <124019197+AyushRai177@users.noreply.github.com>
2024-09-21 23:51:56 +0530

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+//Primitive also known as call by value
+// 7 categories are there in primitive datatype 
+// 7 types : String ,Number ,Boolean , null , undefined , symbol ,BigInt
+
+//javascript is statically typed or dynamically typed ?
+const score = 100
+constScorevalue=100.3
+let UserEmail; 
+const outsidetemp =null
+
+const id =Symbol('1234')
+const anotherid =Symbol('1234')
+console.log(id === anotherid);
+const Bigno=345654357443576654356754n
+
+// Reference type (Non primitive)
+
+//Array, Objects,functions
+const heroes =["Shaktiman","Batman","Spiderman"]
+let myObj={
+    name:"Ayush",
+    age:22,    
+}
+const Myfunction=function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof id);
