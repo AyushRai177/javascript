f2e55102b426b59f7d7e909e8fc056161c875a9e
AyushRai177 <124019197+AyushRai177@users.noreply.github.com>
2024-09-21 23:51:09 +0530

    comparison ofoperators

---
+++ 01_basics/comparison.js
@@ -0,0 +1,13 @@
+/*console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);*/
+// === we should avoid this conversion in most of the course
+
+
+
+
