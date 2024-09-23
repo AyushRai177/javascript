083c9e235b905ed27205ae4de7e9d6e1e51a5959
AyushRai177 <124019197+AyushRai177@users.noreply.github.com>
2024-09-23 23:29:41 +0530

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = "Ayush"
+const repoCount = 50 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('Ayushman-Ayu-com')
+console.log(gameName[1]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
+const newstring=gameName.substring(0,4)
+console.log(newstring);
+const anotherString=gameName.slice(-8 , 4)
+console.log(anotherString);
+
+const newStringOne = (" hitesh ")
+console.log(newStringOne);
+console.log(newStringOne.trimEnd());
+
+const url = "https://ayush.com/ayush%20rai"
+console.log(url.replace('%20','-'));
+console.log(url.includes('aman'));
+console.log(gameName.split('-'));
+
+console.log(gameName.toLocaleUpperCase());
+const x = new String("Hello world");
+console.log(x.valueOf());
+
+
+
+
+
