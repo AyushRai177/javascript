35d88814e25038a9b3ffd90443581a8d22038b3a
AyushRai177 <124019197+AyushRai177@users.noreply.github.com>
2024-09-24 23:44:49 +0530

    nums_and_maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,29 @@
+const num=400
+console.log(num);
+
+const num1 =new Number(400)
+console.log(num1);
+
+console.log(num1.toString().length)
+
+const othername=123.8966
+console.log(othername.toPrecision(3));
+
+const hundreds =1000000
+console.log(hundreds.toLocaleString());
+
+// Maths
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.min(1,2,3,4,5));
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
+
+
+
