69035ac27079b4f0c6cb4575ca08cf71193fb2d9
AyushRai177 <124019197+AyushRai177@users.noreply.github.com>
2024-09-25 23:09:04 +0530

    Dates in javascript

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,42 @@
+// Dates 
+/*let myDate= new Date()
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleString());
+
+
+let myCreatedDate = new Date(2023,11,23)
+let myCreatedDate1 = new Date(2024,11,23,5,3)
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate1.toLocaleString()); 
+
+let myCreatedDate=new Date("01/14/2024")
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp =Date.now()
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+console.log(Date.now());
+
+console.log(Math.floor(Date.now()/1000));
+
+
+newDate.toLocaleString('default' , {
+    weekday: "long"
+})
+
+const today = new Date();
+const birthday = new Date("December 17, 1995 03:24:00"); // DISCOURAGED: may not work in all runtimes
+const birthday2 = new Date("1995-12-17T03:24:00"); // This is standardized and will work reliably
+const birthday3 = new Date(1995, 11, 17); // the month is 0-indexed
+const birthday4 = new Date(1995, 11, 17, 3, 24, 0);
+const birthday5 = new Date(628021800000); // passing epoch timestamp
+console.log(birthday5.toLocaleString());
+*/
+let myTimeStamp =new Date()
+console.log(myTimeStamp.toLocaleString())
+
+
+let my1 =Date.now()
+console.log(my1)
