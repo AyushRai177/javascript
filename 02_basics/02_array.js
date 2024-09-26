de8c4c1c21447888c298e1477d3524b5728b4d26
AyushRai177 <124019197+AyushRai177@users.noreply.github.com>
2024-09-27 02:30:40 +0530

    02_array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,28 @@
+const marvel_heroes = ["thor", "Ironman", "Spiderman"]
+const dc_heroes = ["superman", "flash", "Batman"]
+const rock =['coc','Tr']
+//marvel_heroes.push(dc_heroes)
+//console.log(marvel_heroes[3][1]);
+
+//marvel =marvel_heroes.concat(dc_heroes)
+//console.log(marvel);
+
+const all_newheroes=[...marvel_heroes,...dc_heroes]
+console.log(all_newheroes);
+
+const another_array = [1,2,3,[4,5,6], 7 ,[6,7,[4,5]]]
+const real_array= another_array.flat(Infinity)
+console.log(real_array);
+
+
+console.log(Array .isArray("Ayush"))
+console.log(Array.from("Ayush"));
+console.log(Array.from({name:'Ayush',course:'MCA'}))  //Interesting
+
+
+
+let score = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score,score2,score3));
